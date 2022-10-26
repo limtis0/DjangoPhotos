0dc5b0c879f7bf2b849c00d5eb2c5dde315d2e5d
Vladimir <limtis2109@gmail.com>
2022-10-27 00:02:55 +0200

    Updated tests for create and update

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -1,7 +1,7 @@
 import pytest
 from api.urls import URL
 from api.models import Photo
-from data.data_photos import DataPhotos
+from testdata.data_photos import DataPhotos
 
 
 class TestCRUD:
@@ -11,15 +11,16 @@ class TestCRUD:
         assert response.status_code == 200, 'Failed to read'
 
     @pytest.mark.django_db
-    def test_create(self, api_client):
+    def test_create(self, api_client, photos_cleanup):
         create_fail = api_client.post(f'{URL.API_DIR}{URL.CREATE}', data=DataPhotos.invalid_photo)
         assert create_fail.status_code == 400, 'Bad request is handled incorrectly'
 
         create = api_client.post(f'{URL.API_DIR}{URL.CREATE}', data=DataPhotos.valid_photo)
         assert create.status_code == 200, 'Failed to create'
+        photos_cleanup()
 
     @pytest.mark.django_db
-    def test_update(self, api_client, photo_applied):
+    def test_update(self, api_client, photo_applied, photos_cleanup):
         url = f'{URL.API_DIR}{URL.UPDATE}{photo_applied.id}/'
 
         update_fail = api_client.post(url, data=DataPhotos.invalid_photo)
@@ -27,7 +28,8 @@ class TestCRUD:
 
         update = api_client.post(url, data=DataPhotos.valid_photo_2)
         assert update.status_code == 200, 'Failed to update'
-        assert Photo.get_by_id(1).albumId == DataPhotos.valid_photo_2['albumId'], 'Photo has not changed on update'
+        assert Photo.get_by_id(1).albumId == DataPhotos.valid_photo_2['albumId'], 'Photo info has not changed on update'
+        photos_cleanup()
 
     @pytest.mark.django_db
     def test_delete(self, api_client, photo_applied):
