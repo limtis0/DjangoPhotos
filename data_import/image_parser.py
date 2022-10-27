c4704585f0ea91343f72895ec0338db911d93c05
Vladimir <limtis2109@gmail.com>
2022-10-27 23:15:34 +0200

    Better image parsing method

--- data_import/image_parser.py
+++ data_import/image_parser.py
@@ -1,9 +1,9 @@
 from io import BytesIO
 from PIL import Image
+from base64 import b64decode
 from api.models import PhotoFields
 
 from .webdriver import WebDriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.errorhandler import NoSuchElementException, InvalidArgumentException, WebDriverException
 
 
@@ -12,13 +12,38 @@ class ImageParser:
     def get_image(cls, url: str):
         try:
             WebDriver().driver.get(url)
-            # Takes a screenshot of a first img object found.
-            # TODO: Find a way to download images with Selenium without losing quality
-            screenshot = WebDriver().driver.find_element(By.TAG_NAME, 'img').screenshot_as_png
-            img = Image.open(BytesIO(screenshot))
+
+            # Create a canvas, set it's width and height equal to image's
+            # Write image to canvas, translate to base64
+            # Remove metadata prefix
+            b64img = WebDriver().driver.execute_script(r'''
+                var img = document.getElementsByTagName("img")[0];
+                var canvas = document.createElement("canvas");
+                canvas.width = img.width;
+                canvas.height = img.height;
+                var ctx = canvas.getContext("2d");
+                ctx.drawImage(img, 0, 0);
+                var dataURL = canvas.toDataURL("image/png");
+                return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+            ''')
+
+            # Decode from base64, translate to bytes and write to PIL image
+            img = Image.open(BytesIO(b64decode(b64img)))
             return img
+
         except (NoSuchElementException, InvalidArgumentException, WebDriverException):
             return None
+
+    # @classmethod
+    # def get_image_legacy(cls, url: str):
+    #     try:
+    #         WebDriver().driver.get(url)
+    #         # Takes a screenshot of a first img object found.
+    #         screenshot = WebDriver().driver.find_element(By.TAG_NAME, 'img').screenshot_as_png
+    #         img = Image.open(BytesIO(screenshot))
+    #         return img
+    #     except (NoSuchElementException, InvalidArgumentException, WebDriverException):
+    #         return None
 
     @staticmethod
     def _get_dominant_color(img: Image):
